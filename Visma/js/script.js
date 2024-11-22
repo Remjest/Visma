0962ba86361d6d556a5e7388c14040d16ae8929f
Nisheglov Sergei <Remjest@mail.ru>
2024-11-22 18:41:50 +0700

    adding-general-js
    Добавил JS для шапки и подвала сайта, теперь удобно
    работает навигация по сайту, а так же на маленьких
    экранах в подвале все разделено на открывающиеся
    секции. Для поиска и выбора языка добавлен лишь
    базовый функционал, который позволяет удобно
    взаимодействовать с этими элементами. Сами же они
    еще не выполняют своих функций, это будет добавлено
    в будущем.

---
+++ Visma/js/script.js
@@ -0,0 +1,101 @@
+console.log("Delaem");
+
+let clientWidth = document.body.getBoundingClientRect().width;
+
+if (typeof window.ResizeObserver !== 'undefined') {
+    const resizeObserver = new ResizeObserver(() => {
+        const contentWidth = document.body.getBoundingClientRect().width;
+        if (Math.round(clientWidth * 10) === Math.round(contentWidth * 10)) return;
+        clientWidth = contentWidth;
+    });
+    resizeObserver.observe(document.body);
+}
+
+const headerSearchWrapper = document.querySelector('.header__line-wrapper');
+const headerSearchImg = document.querySelector('.header__search img');
+
+document.addEventListener('click', (e) => {
+    if (clientWidth > 600) return;
+    if (e.target.closest('.header__search img')) {
+        headerSearchWrapper.classList.toggle('header__line-wrapper_active');
+    } else if (!e.target.closest('.header__search')) {
+        headerSearchWrapper.classList.remove('header__line-wrapper_active');
+    }
+});
+
+const headerLangs = document.querySelector('.header__langs');
+
+document.addEventListener('click', (e) => {
+    if (e.target.closest('.header__langs')) {
+        headerLangs.classList.toggle('header__langs_active');
+    } else if (!e.target.closest('.header__langs')) {
+        headerLangs.classList.remove('header__langs_active');
+    }
+});
+
+
+const headerNavPoints = document.querySelectorAll(".header__point");
+const headerNav = document.querySelector(".header__nav");
+
+document.addEventListener('click', (e) => {
+    if (clientWidth > 1000) return;
+    if (e.target.closest('.header__nav')) {
+        if (headerNav.classList.contains('header__nav_active')) {
+            // Закрытие - мгновенное
+            headerNav.style.transition = 'height ease-in 0s';
+        } else {
+            // Открытие - с анимацией
+            headerNav.style.transition = 'height ease-in 0.3s';
+        }
+        headerNav.classList.toggle('header__nav_active');
+    } else if (!e.target.closest('.header__nav')) {
+        // Закрытие - мгновенное
+        headerNav.style.transition = 'height ease-in 0s';
+        headerNav.classList.remove('header__nav_active');
+    }
+});
+
+headerNavPoints.forEach((point) => {
+    point.addEventListener('click', (e) => {
+        e.preventDefault(); // Отменяем стандартное поведение ссылки
+        const link = point.querySelector('a');
+        
+        if (link) {
+            const href = link.getAttribute('href');
+            const anchor = document.querySelector(href);
+            if (anchor) {
+                const dir = href == '#contacts' ? 'end' : 'start';
+                anchor.scrollIntoView({ block: dir, behavior: 'smooth' }); // Прокручиваем к элементу
+            } 
+        }
+    });
+});
+
+
+const footerNavPoints = document.querySelectorAll('.nav');
+const footerNav = document.querySelector('.footer__navs');
+
+footerNav.addEventListener('click', (e) => {
+    if (clientWidth > 1000) return;
+    if (e.target.closest('.nav')) {
+        e.target.closest('.nav').classList.toggle('nav_active');
+    }
+});
+
+if (typeof window.IntersectionObserver !== 'undefined') {
+    const scrollObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting && entry.intersectionRatio >= 0.2) {
+                document.querySelector('.header__point_active').classList.remove('header__point_active');
+                const id = entry.target.getAttribute('id');
+                document.querySelector(`[href='#${id}']`).parentElement.classList.add('header__point_active');
+            }
+        })
+    }, { threshold: 0.2 });
+
+    const scrollers = document.querySelectorAll('.scroller');
+    scrollers.forEach((scroller) => {
+        scrollObserver.observe(scroller);
+    });
+}
+
